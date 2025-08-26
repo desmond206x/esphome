a020fab04707797d4f53ae1bb85353c9a40ef20a
desmond206x <desmond206x@gmail.com>
2025-08-26 22:30:16 +0200

    .

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4788,10 +4788,10 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       //   this->data(this->old_buffer_[i]);
       // }
 
-      this->command(0x10);
+      this->command(0x13);
       delay(2);
       for (uint32_t i = 0; i < buf_len; i++) {
-        this->data(this->buffer_[i]);
+        this->data(~this->buffer_[i]);
       }
 
       // for (size_t i = 0; i < this->get_buffer_length_(); i++) {
