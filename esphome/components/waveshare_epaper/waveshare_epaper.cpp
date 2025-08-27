d311b4d372e9b56529d7f19d5552f73b763dbafa
desmond206x <desmond206x@gmail.com>
2025-08-27 23:27:40 +0200

    old

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4802,11 +4802,11 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
 
     // this->data(0x01);
 
-      // this->command(0x10);
-      // delay(2);
-      // for (uint32_t i = 0; i < buf_len; i++) {
-      //   this->data(this->old_buffer_[i]);
-      // }
+      this->command(0x10);
+      delay(2);
+      for (uint32_t i = 0; i < buf_len; i++) {
+        this->data(this->old_buffer_[i]);
+      }
 
       this->command(0x13);
       delay(2);
@@ -4814,11 +4814,11 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
         this->data(~this->buffer_[i]);
       }
 
-      // this->command(0x11);
+      this->command(0x11);
 
-      // for (size_t i = 0; i < this->get_buffer_length_(); i++) {
-      //   this->old_buffer_[i] = this->buffer_[i];
-      // }
+      for (size_t i = 0; i < this->get_buffer_length_(); i++) {
+        this->old_buffer_[i] = this->buffer_[i];
+      }
 
       this->turn_on_display_();
 
