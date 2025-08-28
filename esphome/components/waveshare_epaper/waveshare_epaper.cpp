2be41cf605d42fd5a69854c84b93880fa618ecf9
desmond206x <desmond206x@gmail.com>
2025-08-28 21:30:40 +0200

    .

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4780,7 +4780,7 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       // this->wait_until_idle_();
     // } else {
       ESP_LOGI(TAG, "Partial refresh");
-      // this->init_display_partial_();
+      this->init_display_partial_();
 
       // Enable partial refresh
       this->command(0xE5);
@@ -4809,25 +4809,31 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       // for (uint32_t i = 0; i < buf_len; i++) {
       //   this->data(this->old_buffer_[i]);
       // }
+
+      this->command(0x10);
+      delay(2);
+      for (uint32_t i = 0; i < buf_len; i++) {
+        this->data(0xff);
+      }
       
       // Idee: 0xFF oder so als Basis zu schreiben und dann nur den Buffer dazupacken für das partial Update
       this->command(0x13);
       delay(2);
       for (uint32_t i = 0; i < buf_len; i++) {
-        this->data(0xFF);
-        // this->data(this->buffer_[i]);
+        // this->data(0xFF);
+        this->data(this->buffer_[i]);
         // this->old_buffer_[i] = this->buffer_[i];
       }
 
-      this->command(0x11);
+      // this->command(0x11);
       
       this->turn_on_display_();
       
       this->wait_until_idle_();
 
-      this->command(0x92);
+      // this->command(0x92);
 
-      this->wait_until_idle_();
+      // this->wait_until_idle_();
     // }
   }
 
