1f56bdff3329ac0cbe7de49375ff84592e974c76
desmond206x <desmond206x@gmail.com>
2025-08-29 08:11:25 +0200

    k

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4908,18 +4908,17 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
 
       this->command(0x10);
       for (uint32_t i = 0; i < buf_len; i++) {
-        this->data(0x00);
-        // this->data(this->old_buffer_[i]);
+        this->data(this->old_buffer_[i]);
       }
       
       // Idee: 0xFF oder so als Basis zu schreiben und dann nur den Buffer dazupacken für das partial Update
       this->command(0x13);
       for (uint32_t i = 0; i < buf_len; i++) {
         // this->data(0xFF);
-        this->data(this->buffer_[i]);
+        this->data(old_buffer_[i] ^ this->buffer_[i]);
         this->old_buffer_[i] = this->buffer_[i];
       }
-
+      
       // this->command(0x11);
       
       this->turn_on_display_();
