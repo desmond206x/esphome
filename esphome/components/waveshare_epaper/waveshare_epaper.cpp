e6e5a49a824139d2be2979b1206891a4577a17a2
desmond206x <desmond206x@gmail.com>
2025-08-29 08:48:32 +0200

    off

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4921,9 +4921,9 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       
       // this->command(0x11);
       
-      this->turn_on_display_();
-      delay(100);  // NOLINT
-      this->wait_until_idle_();
+      // this->turn_on_display_();
+      // delay(100);  // NOLINT
+      // this->wait_until_idle_();
 
       this->command(0x92);
 
