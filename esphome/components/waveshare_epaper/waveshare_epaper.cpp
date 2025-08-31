db95ea3952f71e53b1bc0b1be0880c613912a63c
desmond206x <desmond206x@gmail.com>
2025-08-31 22:04:51 +0200

    ~

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4972,7 +4972,7 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
         // this->data(0xFF);
         // this->data(old_buffer_[i] ^ this->buffer_[i]);
         // this->old_buffer_[i] = this->buffer_[i];
-        this->data(this->buffer_[i]);
+        this->data(~this->buffer_[i]);
       }
       
       // this->command(0x15);
