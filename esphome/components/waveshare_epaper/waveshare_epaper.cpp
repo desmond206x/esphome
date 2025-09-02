6c30b84557af092e809596033d136342a1e5869e
desmond206x <desmond206x@gmail.com>
2025-09-02 22:03:34 +0200

    old

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4553,16 +4553,16 @@ static const uint8_t PART_UPDATE_LUT_TTGO_DKE[LUT_SIZE_TTGO_DKE_PART] = {
 void WaveshareEPaper7P5InBV3PBWR::initialize() { 
   this->init_display_(); 
 
-  // // old buffer for partial update
-  // RAMAllocator<uint8_t> allocator;
-  // this->old_buffer_ = allocator.allocate(this->get_buffer_length_() / 2);
-  // if (this->old_buffer_ == nullptr) {
-  //   ESP_LOGE(TAG, "Could not allocate old buffer for display!");
-  //   return;
-  // }
-  // for (size_t i = 0; i < this->get_buffer_length_() / 2; i++) {
-  //   this->old_buffer_[i] = 0xFF;
-  // }
+  // old buffer for partial update
+  RAMAllocator<uint8_t> allocator;
+  this->old_buffer_ = allocator.allocate(this->get_buffer_length_() / 2);
+  if (this->old_buffer_ == nullptr) {
+    ESP_LOGE(TAG, "Could not allocate old buffer for display!");
+    return;
+  }
+  for (size_t i = 0; i < this->get_buffer_length_() / 2; i++) {
+    this->old_buffer_[i] = 0xFF;
+  }
 }
 bool WaveshareEPaper7P5InBV3PBWR::wait_until_idle_() {
   if (this->busy_pin_ == nullptr) {
@@ -4878,7 +4878,7 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
     delay(2); //TODO: Remove
     for (uint32_t i = 0; i < buf_len; i++) {
       this->data(~this->buffer_[i]);
-      // this->old_buffer_[i] = this->buffer_[i];
+      this->old_buffer_[i] = this->buffer_[i];
     }
 
     this->command(0x13);  // Send red data Transmission
@@ -4970,7 +4970,7 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       this->command(0x13);
       for (uint32_t i = 0; i < buf_len; i++) {
         // this->data(0xFF);
-        // this->data(old_buffer_[i] ^ this->buffer_[i]);
+        this->data(old_buffer_[i] ^ this->buffer_[i]);
         // this->old_buffer_[i] = this->buffer_[i];
         this->data(this->buffer_[i]);
       }
