f799f8b51a6f81010e2b0dd22f674d583e4ed4e4
desmond206x <desmond206x@gmail.com>
2025-09-09 22:26:19 +0200

    tdeh

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4974,11 +4974,20 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       //   this->data(~this->buffer_[i]);
       // }
 
-      // this->command(0x10);
-      // delay(2);
-      // for (uint32_t i = 0; i < buf_len; i++) {
-      //   this->data(0xFF);
-      // }
+      this->command(0x10);
+      ESP_LOGI(TAG, "0x10 send");
+      delay(2);
+      this->start_data_();
+      ESP_LOGI(TAG, "0x10 data started");
+      for (uint32_t i = 0; i < buf_len; i++) {
+        this->data(0xFF);
+      }
+      ESP_LOGI(TAG, "0x13 data send");
+      this->end_data_();
+      ESP_LOGI(TAG, "0x10 data ended");
+
+      this->command(0x92);
+      ESP_LOGI(TAG, "0x92 send");
 
       // this->command(0x11);
 
@@ -4987,22 +4996,22 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
 
       delay(2);
       this->start_data_();
-      ESP_LOGI(TAG, "data started");
+      ESP_LOGI(TAG, "0x13 data started");
 
       for (uint32_t i = 0; i < buf_len; i++) {
         // this->data(0xFF);
         // this->data(old_buffer_[i] ^ this->buffer_[i]);
         // this->old_buffer_[i] = this->buffer_[i];
-        this->write_array(this->buffer_, buf_len);
+        this->write_array(~this->buffer_, buf_len);
         // this->data(~this->buffer_[i]);
       }
       ESP_LOGI(TAG, "0x13 data send");
 
       this->end_data_();
-      ESP_LOGI(TAG, "data ended");
+      ESP_LOGI(TAG, "0x13 data ended");
       
-      this->command(0x92);
-      ESP_LOGI(TAG, "0x92 send");
+      // this->command(0x92);
+      // ESP_LOGI(TAG, "0x92 send");
 
       // this->command(0x15);
       // this->data(0x00);
