a2457cc415e2418859a222684d6d15cba9e4790e
desmond206x <desmond206x@gmail.com>
2025-09-10 21:24:22 +0200

    uff

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -5001,18 +5001,17 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       delay(200);  // NOLINT
       this->wait_until_idle_();
 
-      delay(2);
       // this->start_data_();
       // delay(2);
       // ESP_LOGI(TAG, "0x13 data started");
-
-      for (uint32_t i = 0; i < buf_len; i++) {
-        // this->data(0xFF);
-        // this->data(old_buffer_[i] ^ this->buffer_[i]);
-        // this->old_buffer_[i] = this->buffer_[i];
-        this->write_array(this->buffer_, buf_len);
-        // this->data(~this->buffer_[i]);
-      }
+      
+      this->write_array(this->buffer_, buf_len);
+      // for (uint32_t i = 0; i < buf_len; i++) {
+      //   // this->data(0xFF);
+      //   // this->data(old_buffer_[i] ^ this->buffer_[i]);
+      //   // this->old_buffer_[i] = this->buffer_[i];
+      //   this->data(~this->buffer_[i]);
+      // }
       ESP_LOGI(TAG, "0x13 data send");
 
       // this->end_data_();
