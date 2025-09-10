c22ba6f97b2712256301d0d8b5d1de69ce145af6
desmond206x <desmond206x@gmail.com>
2025-09-10 21:04:20 +0200

    startend

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4976,18 +4976,19 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
 
       this->command(0x10);
       ESP_LOGI(TAG, "0x10 send");
-      delay(2);
-      this->start_data_();
-      delay(2);
-      ESP_LOGI(TAG, "0x10 data started");
+      delay(200);  // NOLINT
+      this->wait_until_idle_();
+      // this->start_data_();
+      // delay(2);
+      // ESP_LOGI(TAG, "0x10 data started");
       for (uint32_t i = 0; i < buf_len; i++) {
         this->data(0xFF);
       }
       delay(2);
       ESP_LOGI(TAG, "0x13 data send");
-      this->end_data_();
-      delay(2);
-      ESP_LOGI(TAG, "0x10 data ended");
+      // this->end_data_();
+      // delay(2);
+      // ESP_LOGI(TAG, "0x10 data ended");
 
       this->command(0x92);
       ESP_LOGI(TAG, "0x92 send");
@@ -4998,9 +4999,9 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       ESP_LOGI(TAG, "0x13 send");
 
       delay(2);
-      this->start_data_();
-      delay(2);
-      ESP_LOGI(TAG, "0x13 data started");
+      // this->start_data_();
+      // delay(2);
+      // ESP_LOGI(TAG, "0x13 data started");
 
       for (uint32_t i = 0; i < buf_len; i++) {
         // this->data(0xFF);
@@ -5011,9 +5012,9 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
       }
       ESP_LOGI(TAG, "0x13 data send");
 
-      this->end_data_();
-      delay(2);
-      ESP_LOGI(TAG, "0x13 data ended");
+      // this->end_data_();
+      // delay(2);
+      // ESP_LOGI(TAG, "0x13 data ended");
       
       // this->command(0x92);
       // ESP_LOGI(TAG, "0x92 send");
