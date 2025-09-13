2bae7f20c4890712e26cf9f9fb87d08b5be2c565
desmond206x <desmond206x@gmail.com>
2025-09-13 22:51:06 +0200

    ,000

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -5009,7 +5009,7 @@ void HOT WaveshareEPaper7P5InBV3PBWR::display() {
         ESP_LOGI(TAG, "0x10 send");
         delay(2);
         for (uint32_t i = 0; i < buf_len; i++) {
-          this->data(0xFF);
+          this->data(0x00);
         }
         ESP_LOGI(TAG, "0x10 data send");
       }
